d5d971bda4f0b80d746e2eb98bb0d4f2ce89c301
Logon Aluno <logonrmlocal@fiap.com.br>
2023-04-18 20:56:37 -0300

    Lista feita

--- lib/lista_contatos.dart
+++ lib/lista_contatos.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:listview_demo/contato.dart';
 
 class ListaContatos extends StatefulWidget {
   const ListaContatos({Key? key}) : super(key: key);
@@ -8,10 +9,31 @@ class ListaContatos extends StatefulWidget {
 }
 
 class _ListaContatosState extends State<ListaContatos> {
+  List<Contato> contatos = [
+    Contato('Eduardo Varela', 'eduardo@mail.com'),
+    Contato('Theo Varela', 'theo@mail.com'),
+    Contato('Ellen Varela', 'ellen@mail.com'),
+    Contato('Amanda Varela', 'amanda@mail.com'),
+    Contato('Roberto Varela', 'roberto@mail.com'),
+    Contato('Fran Varela', 'fran@mail.com'),
+    Contato('Carlos Varela', 'carlos@mail.com'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Contatos')),
+      appBar: AppBar(
+        title: Text('Contatos'),
+      ),
+      body: ListView.builder(
+          itemCount: contatos.length,
+          itemBuilder: (BuildContext contex, int index) {
+            return ListTile(
+              leading: CircleAvatar(),
+              title: Text(contatos[index].nomeCompleto),
+              subtitle: Text(contatos[index].email),
+            );
+          }),
     );
   }
 }
